a08a3c8422546eddf9be835fad9ba746daed9dd7
lanza <dev@refresh.blue>
2017-10-20 17:08:21 +0200

    Added biguints, and movement

---
+++ code/main.hpp
@@ -0,0 +1,16 @@
+#ifndef main_h
+#define main_h
+
+#include "world.hpp"
+
+struct Player {
+    PictureCoords *coords[2];
+    Vec2 position_relative_to_coords;
+    Vec2 v;
+    Vec3 looking_direction;
+    bool falling;
+};
+
+extern Player player;
+
+#endif /* main_h */
